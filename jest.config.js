4dd0f0786dd2bf538a2c26f505d279bbbe9d2032
Oleksandr <sashko1921@gmail.com>
2025-07-25 10:59:01 +0300

    chore: add jest config

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+export default {
+  rootDir: './',
+  testEnvironment: 'node',
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.mjs?$',
+  clearMocks: true,
+  testPathIgnorePatterns: ['ignored'],
+  transform: {},
+};
